688b7441fe12e18ffbc68046e83eb18f335abb71
wlllssd <511872591@qq.com>
2019-03-13 15:00:55 +0800

    20190913

--- test.py
+++ test.py
@@ -1,18 +1,16 @@
-def valid(i,j,row,col):
-        pos = [[i,j],[i-1,j],[i+1,j],[i,j+1],[i,j-1]]
-        print(pos)
-        if i-1 < 0:
-            pos.remove([i-1,j])
-        if j-1 < 0:
-            pos.remove([i,j-1])
-        if i+1 >= row:
-            pos.remove([i+1,j])
-        if j+1 >= col:
-            pos.remove([i,j+1])
-        return pos
-a = valid(3,3,4,5)
-for each in a:
-    print(each)
-    for ele in each:
-        print(ele)
-print(a)
+res = []
+def fx(a,begin,end):
+    if begin == end:
+        res.append(a.copy())
+    else:
+        for i in range(begin,end):
+            a[i],a[begin] = a[begin],a[i]
+            fx(a,begin+1,end)
+            a[begin],a[i] = a[i],a[begin]
+def A(N):
+    a = [i for i in range(1,N+1)]
+    fx(a,0,N)
+
+A(3)
+print(res)
+print(len(res))
