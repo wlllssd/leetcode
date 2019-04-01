93538c2881a8654a78f93b6fae59006be1b606e9
wlllssd <511872591@qq.com>
2019-04-01 11:51:14 +0800

    20190401

---
+++ 40.combination-sum-ii.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode id=40 lang=python3
+#
+# [40] Combination Sum II
+#
+# https://leetcode.com/problems/combination-sum-ii/description/
+#
+# algorithms
+# Medium (40.04%)
+# Total Accepted:    208.1K
+# Total Submissions: 511.9K
+# Testcase Example:  '[10,1,2,7,6,1,5]\n8'
+#
+# Given a collection of candidate numbers (candidates) and a target number
+# (target), find all unique combinations in candidates where the candidate
+# numbers sums to target.
+# 
+# Each number in candidates may only be used once in the combination.
+# 
+# Note:
+# 
+# 
+# All numbers (including target) will be positive integers.
+# The solution set must not contain duplicate combinations.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: candidates = [10,1,2,7,6,1,5], target = 8,
+# A solution set is:
+# [
+# ⁠ [1, 7],
+# ⁠ [1, 2, 5],
+# ⁠ [2, 6],
+# ⁠ [1, 1, 6]
+# ]
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: candidates = [2,5,2,1,2], target = 5,
+# A solution set is:
+# [
+# [1,2,2],
+# [5]
+# ]
+# 
+# 
+#
+class Solution:
+    def dfs(self, nums, start, path, target, res):
+        if target == 0:
+            res.append(path)
+            return
+        for i in range(start,len(nums)):
+            if nums[i] > target:
+                break
+            self.dfs(nums, i+1, path.append(nums[i]), target-nums[i], res)
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        self.dfs(candidates,0,[],target,res)
+        return res
+
+
+
