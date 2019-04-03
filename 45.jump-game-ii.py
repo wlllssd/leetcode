9c5cc0b47eb1391c0d84e18b00182839b7fb3cda
wlllssd <511872591@qq.com>
2019-04-03 21:48:10 +0800

    20190403

---
+++ 45.jump-game-ii.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=45 lang=python3
+#
+# [45] Jump Game II
+#
+# https://leetcode.com/problems/jump-game-ii/description/
+#
+# algorithms
+# Hard (27.39%)
+# Total Accepted:    160.4K
+# Total Submissions: 578.7K
+# Testcase Example:  '[2,3,1,1,4]'
+#
+# Given an array of non-negative integers, you are initially positioned at the
+# first index of the array.
+# 
+# Each element in the array represents your maximum jump length at that
+# position.
+# 
+# Your goal is to reach the last index in the minimum number of jumps.
+# 
+# Example:
+# 
+# 
+# Input: [2,3,1,1,4]
+# Output: 2
+# Explanation: The minimum number of jumps to reach the last index is 2.
+# ⁠   Jump 1 step from index 0 to 1, then 3 steps to the last index.
+# 
+# Note:
+# 
+# You can assume that you can always reach the last index.
+# 
+#
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        level = 0
+        lastMax = 0
+        currentMax = 1
+        while currentMax < len(nums):
+            level += 1
+            nextMax = currentMax
+            for i in range(lastMax,currentMax):
+                nextMax = max(nextMax,i+nums[i])
+            lastMax = currentMax
+            currentMax = nextMax
+        return level
